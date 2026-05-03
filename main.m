ca21e2f1cc6c7edcbcc242ef6b3e3c9c86cec5c5
Andynium <ender3info@gmail.com>
2026-05-03 14:14:50 +0200

    first own strucure

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  LeoLibrary
+//
+//  Created by Administrator on 03.05.26.
+//  Copyright __MyCompanyName__ 2026. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
